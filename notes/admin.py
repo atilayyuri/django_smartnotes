2fb8af1186a2e9c5f536c3f066ddc41de613f016
atilayyuri <tamkan.atilay@gmail.com>
2023-06-16 16:02:41 +0200

    enabling notes model at admin interface

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+
+class NotesAdmin(admin.ModelAdmin):
+    # to display the title of the note
+    list_display = ('title',)
+    pass
+
+# enabling notes model at admin interface
+admin.site.register(models.Notes, NotesAdmin)
--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+# some methods for objects of the model
+# Notes.objects.create
+# Notes.objects.all()
+# Notes.objects.get(pk='#')
+# Notes.objects.filter(title_startswith='first')
+# Notes.objects.filter(text__icontains='django')
+# Notes.objects.exclude(text__icontains='django')
+class Notes(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    # auto_now_add --> everytime a note is created
+    # created field is populated with time that note
+    # is created
+    created = models.DateTimeField(auto_now_add=True)
+
+
