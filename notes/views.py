b3639cd6b0037d980c0534f7c53db2b0297d2779
atilayyuri <tamkan.atilay@gmail.com>
2023-06-19 20:45:07 +0200

    adding post option to create notes, creating form for validation

--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 
 from .models import Notes
-
+from .forms import NotesForm
 
 # Create your views here.
+class NotesCreateView(CreateView):
+    model = Notes
+    success_url = "/smart/notes"
+    form_class = NotesForm
 
 
 class NotesListView(ListView):
