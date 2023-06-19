ec5516f15d0e946962b1e8a923773b5ae953cd81
atilayyuri <tamkan.atilay@gmail.com>
2023-06-19 20:50:31 +0200

    validating new post via if it has 'Django' string inside

--- notes/forms.py
+++ notes/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
 from .models import Notes
 
 
@@ -10,3 +10,6 @@ class NotesForm(forms.ModelForm):
 
     def clean_title(self):
         title = self.cleaned_data['title']
+        if 'Django' not in title:
+            raise ValidationError("We only accept notes about Django!")
+        return title
